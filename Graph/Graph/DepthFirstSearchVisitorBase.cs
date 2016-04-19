d2cda5f95b72a921c47a073871d1cb4af4effb76
MBulli <markusbullmann@gmail.com>
2016-04-19 16:05:12 +0200

    Added DFS-Visitor

---
+++ Graph/Graph/DepthFirstSearchVisitorBase.cs
@@ -0,0 +1,45 @@
+﻿using QuickGraph.Algorithms.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    abstract class DepthFirstSearchVisitorBase
+    {
+        DepthFirstSearchAlgorithm<DataVertex, DataEdge> algo;
+
+        public DepthFirstSearchVisitorBase(DepthFirstSearchAlgorithm<DataVertex, DataEdge> algo)
+        {
+            if (algo == null)
+                throw new ArgumentNullException(nameof(algo));
+
+            algo.InitializeVertex += Algo_InitializeVertex;
+            algo.DiscoverVertex += Algo_DiscoverVertex;
+            algo.FinishVertex += Algo_FinishVertex;
+
+            algo.Started += Algo_Started;
+            algo.Finished += Algo_Finished;
+            algo.Aborted += Algo_Aborted;
+        }
+        
+        protected virtual void SearchStarted() { }
+        protected virtual void SearchFinished() { }
+        protected virtual void SearchAborted() { }
+
+        protected virtual void InitializeVertex(DataVertex vertex) { }
+        protected virtual void DiscoverVertex(DataVertex vertex) { }
+        protected virtual void FinishVertex(DataVertex vertex) { }
+
+
+
+        private void Algo_InitializeVertex(DataVertex vertex) { InitializeVertex(vertex); }
+        private void Algo_Started(object sender, EventArgs e) { SearchStarted(); }
+        private void Algo_FinishVertex(DataVertex vertex) { FinishVertex(vertex); }
+        private void Algo_Finished(object sender, EventArgs e) { SearchFinished(); }
+        private void Algo_DiscoverVertex(DataVertex vertex) { DiscoverVertex(vertex); }
+        private void Algo_Aborted(object sender, EventArgs e) { SearchAborted(); }
+    }
+}
