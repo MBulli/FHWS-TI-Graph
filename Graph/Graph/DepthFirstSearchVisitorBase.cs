bb8f2875eeb987423f58149a7bafdb9ce7776a24
MBulli <markusbullmann@gmail.com>
2016-04-19 17:02:35 +0200

    Added LambdaVisitor

--- Graph/Graph/DepthFirstSearchVisitorBase.cs
+++ Graph/Graph/DepthFirstSearchVisitorBase.cs
@@ -13,12 +13,17 @@ namespace Graph
 
         public DepthFirstSearchVisitorBase(DataGraph grapToVisit)
             : this(new DepthFirstSearchAlgorithm<DataVertex, DataEdge>(grapToVisit))
-        { }
+        {
+            if (grapToVisit == null)
+                throw new ArgumentNullException(nameof(grapToVisit));
+        }
 
         public DepthFirstSearchVisitorBase(DepthFirstSearchAlgorithm<DataVertex, DataEdge> algo)
         {
             if (algo == null)
                 throw new ArgumentNullException(nameof(algo));
+
+            this.algo = algo;
 
             algo.InitializeVertex += Algo_InitializeVertex;
             algo.DiscoverVertex += Algo_DiscoverVertex;
@@ -50,5 +55,54 @@ namespace Graph
         private void Algo_Finished(object sender, EventArgs e) { SearchFinished(); }
         private void Algo_DiscoverVertex(DataVertex vertex) { DiscoverVertex(vertex); }
         private void Algo_Aborted(object sender, EventArgs e) { SearchAborted(); }
+    }
+
+    abstract class DepthFirstSearchLambdaVisitorBase
+    {
+        DepthFirstSearchAlgorithm<DataVertex, DataEdge> algo;
+
+        public DepthFirstSearchLambdaVisitorBase(DataGraph grapToVisit)
+            : this(new DepthFirstSearchAlgorithm<DataVertex, DataEdge>(grapToVisit))
+        {
+            if (grapToVisit == null)
+                throw new ArgumentNullException(nameof(grapToVisit));
+        }
+
+        public DepthFirstSearchLambdaVisitorBase(DepthFirstSearchAlgorithm<DataVertex, DataEdge> algo)
+        {
+            if (algo == null)
+                throw new ArgumentNullException(nameof(algo));
+
+            this.algo = algo;
+
+            algo.InitializeVertex += Algo_InitializeVertex;
+            algo.DiscoverVertex += Algo_DiscoverVertex;
+            algo.FinishVertex += Algo_FinishVertex;
+
+            algo.Started += Algo_Started;
+            algo.Finished += Algo_Finished;
+            algo.Aborted += Algo_Aborted;
+        }
+
+        public void Start()
+        {
+            algo.Compute();
+        }
+
+        public Action SearchStarted { get; set; }
+        public Action SearchFinished { get; set; }
+        public Action SearchAborted { get; set; }
+
+        public Action<DataVertex> InitializeVertex { get; set; }
+        public Action<DataVertex> DiscoverVertex { get; set; }
+        public Action<DataVertex> FinishVertex { get; set; }
+
+
+        private void Algo_InitializeVertex(DataVertex vertex) { InitializeVertex?.Invoke(vertex); }
+        private void Algo_Started(object sender, EventArgs e) { SearchStarted?.Invoke(); }
+        private void Algo_FinishVertex(DataVertex vertex) { FinishVertex(vertex); }
+        private void Algo_Finished(object sender, EventArgs e) { SearchFinished?.Invoke(); }
+        private void Algo_DiscoverVertex(DataVertex vertex) { DiscoverVertex?.Invoke(vertex); }
+        private void Algo_Aborted(object sender, EventArgs e) { SearchAborted?.Invoke(); }
     }
 }
