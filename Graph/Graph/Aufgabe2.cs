911096ce610a5fe1fc8f3892195e4bd056917a8b
Stefan Gerasch <stefangerasch1991@gmail.com>
2016-04-19 17:29:05 +0200

    Added EulerProblemSolver

---
+++ Graph/Graph/Aufgabe2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph {
+    class Aufgabe2 {
+        public static void A() {
+            string euler1 = @"TestFiles\Euler1.txt";
+            string euler2 = @"TestFiles\Euler2.txt";
+
+            //A(euler1);
+            A(euler2);
+
+        }
+        private static void A(string path) {
+            Dictionary<string, DataVertex> vertices = new Dictionary<string, DataVertex>();
+            List<DataEdge> edges = new List<DataEdge>();
+            FileParser.parse(path, ref vertices, ref edges);
+
+            var graph = new DataGraph(vertices.Values, edges);
+            EulerProblemSolver euler = new EulerProblemSolver(graph);
+            Debug.WriteLine("EulerSolver for File: " + path);
+            Debug.WriteLine("Kreis: " + euler.IsEulerKreis);
+            Debug.WriteLine("Pfad: " + euler.IsEulerPfad);
+        }
+    }
+}
---
+++ Graph/Graph/EulerProblemSolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph {
+    class EulerProblemSolver : DepthFirstSearchVisitorBase {
+        private DataGraph graph;
+        public bool IsEulerKreis { get; private set; }
+        public bool IsEulerPfad { get; private set; }
+
+
+        public EulerProblemSolver(DataGraph graph) : base(graph) {
+            this.graph = graph;
+            JustDoIt();
+        }
+
+        List<DataVertex> visitedEven = new List<DataVertex>();
+        List<DataVertex> visitedUnEven = new List<DataVertex>();
+        protected override void DiscoverVertex(DataVertex vertex) {
+            base.DiscoverVertex(vertex);
+            if (graph.Degree(vertex) % 2 == 0) {
+                visitedEven.Add(vertex);
+            } else {
+                visitedUnEven.Add(vertex);
+            }
+        }
+        private void JustDoIt() {
+            this.Start();
+            // wenn nicht zusammenhängender Graph dann weder Weg noch Kreis.
+            if (visitedEven.Count + visitedUnEven.Count != graph.Vertices.Count()) {
+                IsEulerKreis = false;
+                IsEulerPfad = false;
+            } else {
+                if (visitedUnEven.Count == 0) {
+                    // Wenn kein Knoten ungeraden Grads ist, ist der Graph ein EulerKreis und ein EulerPfad.
+                    IsEulerKreis = true;
+                    IsEulerPfad = true;
+                } else if (visitedUnEven.Count == 2) {
+                    // Wenn zwei Knoten ungeraden Grads sind, ist der Graph ein Eulerpfad.
+                    IsEulerPfad = true;
+                }
+            }
+        }
+    }
+}
